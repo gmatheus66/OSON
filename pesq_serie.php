b3fca56b8f0b7af058000641d33c503687fd163e
Matheus G <gmatheus66@hotmail.com>
2019-08-18 04:04:44 -0300

    GH-6 modificação do select pra retornar tbm a média da nota da série

--- pesq_serie.php
+++ pesq_serie.php
@@ -4,11 +4,14 @@ include "conect.php";
 $serie = $_POST["Serie"];
 
 
-$result = $conn -> prepare("SELECT name,genre,seasons,synopsis,age_range,lauch_year, main_cast FROM series WHERE name = ?");
+//Search Series by name
+$result = $conn -> prepare("SELECT series.id, series.name, series.genre, series.seasons, series.synopsis, series.age_range, series.lauch_year, series.main_cast, CAST(AVG(note_serie) AS DECIMAL(10,2)) as rating
+FROM series ,users_series WHERE series.id = users_series.serie_id AND series.name = ? GROUP BY serie_id");
 $result -> bindParam(1,$serie);
 $result -> execute();
 $ser = $result -> fetch();
 
+var_dump($ser);
 echo json_encode($ser);
 
 ?>
