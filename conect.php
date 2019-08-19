2efe0e9bab2403bb3d4d9918868207ca35bb2d4e
GuiLira <liraguilherme252@gmail.com>
2019-08-19 13:06:26 -0300

    Adição de variavel global, .

--- conect.php
+++ conect.php
@@ -4,6 +4,8 @@ $dbname = "pep20192series";
 $user = "pep20192";
 $passw = "pep20192";
 
+global $conn;
+
 try {
 	$conn = new PDO("mysql:host=localhost;dbname=".$dbname,$user, $passw);
 	// echo"deu bom";
