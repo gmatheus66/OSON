a9f995a02732f397212127b087cb6d956149a9d8
Matheus G <gmatheus66@hotmail.com>
2019-08-20 23:32:17 -0300

    GH-6 Mudança da query do select por genero

--- pesq_serie.php
+++ pesq_serie.php
@@ -48,11 +48,11 @@ if(isset($serie) && empty($select)){
 }
 if(isset($select) && empty($serie)){
 
-    $result = $conn -> prepare("SELECT series.id, series.name, series.genre, series.seasons, series.synopsis, series.age_range, series.lauch_year, series.main_cast, CAST(AVG(note_serie) AS DECIMAL(10,2)) as rating
-    FROM series ,users_series WHERE series.id = users_series.serie_id AND lower(series.genre) = lower(?) GROUP BY serie_id");
+    
+$result = $conn -> prepare("SELECT series.id, series.name, series.genre, series.seasons, series.synopsis, series.age_range, series.lauch_year, series.main_cast, CAST(AVG(users_series.note_serie) AS DECIMAL(10,2)) AS NOTA FROM users_series INNER JOIN series ON users_series.serie_id = series.id WHERE lower(series.genre) = lower(?) GROUP BY series.id, series.name, series.genre, series.seasons, series.synopsis, series.age_range, series.lauch_year, series.main_cast;");
     $result -> bindParam(1,$select);
     $result -> execute();
-    $ser = $result -> fetch();
+    $ser = $result -> fetchAll();
     
     echo json_encode($ser);
     //var_dump($ser);
