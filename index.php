82a558c8ec76203dde4c00feb9a1e0ad9dda7d91
siilvatamires <tamiresmaria561@gmail.com>
2019-08-27 08:35:53 -0300

    GH-7 Exclusão da index.html e correção da index.php

--- index.php
+++ index.php
@@ -1,68 +1,92 @@
-<?php 
-include "conect.php";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <title>OSON Séries</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <nav id="inicio" class="navBar" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="#" class="brand-logo">OSON</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="cadastrar.php">Cadastrar</a></li>
+        <li><a href="login.php">Entrar</a></li>
+      </ul>
+      <ul id="nav-mobile" class="sidenav">
+        <li><a href="#">Cadastrar</a></li>
+        <li><a href="#">Entrar</a></li>
+      </ul>
+      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+
+  <div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <br><br>
+        <h1 class="header center teal-text text-lighten-2 tituloTxt">OSON Séries</h1>
+        <div class="row center">
+          <h5 class="header col s12 light txt">Muitas séries para assistir e você acaba se perdendo?<br> Logo você pensa "Oh shit Oh no, em que episódio estava?" <br>Seus problemas acabaram, chegou o OSON SERIES 2000 ULTRA</h5>
+        </div>
+        <br><br>
+
+      </div>
+    </div>
+    <div class="parallax"><img src="1.jpg" alt="Unsplashed background img 1"></div>
+  </div>
+
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center">
+          <h5 class="header col s12 light txt">Com a OSON você consegue guardar onde parou nas suas séries.<br><br>Evitando assim a perda de tempo para continuar a ver suas programações favoritas.</h5>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="2.jpg" alt="Unsplashed background img 2"></div>
+  </div>
 
 
-$serie = $_POST["Serie"];
-$select = $_GET["select_genero"];
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <div class="row center">
+          <h5 class="header col s12 light txt">Avalie e nos diga o que achou da série que está acompanhando.<br><br> Assim, os usuários podem analisar qual será seu próximo vício.</h5>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="3.jpg" alt="Unsplashed background img 3"></div>
+  </div>
 
-var_dump($select);
-
-
-if($select == "Genero"){
-    header('HTTP/1.1 500 Internal Server Booboo');
-    header('Content-Type: application/json; charset=UTF-8');
-    echo die(json_encode(array('message' => 'ERROR', 'code' => 1337)));
-    exit();
-}
-if(!empty($select) && !empty($serie)){
-
-    $result = $conn -> prepare("SELECT series.id, series.name, series.genre, series.seasons, series.synopsis, series.age_range, series.lauch_year, series.main_cast, CAST(AVG(note_serie) AS DECIMAL(10,2)) as rating
-    FROM series ,users_series WHERE series.id = users_series.serie_id AND series.genre = ? AND  series.name = ? GROUP BY serie_id");
-    $result -> bindParam(1,$select);
-    $result -> bindParam(2, $select);
-    $result -> execute();
-    $ser = $result -> fetch();
-    if(!isset($ser)){
-        echo json_encode($ser);
-    }else{
-        header('HTTP/1.1 500 Internal Server Booboo');
-        header('Content-Type: application/json; charset=UTF-8');
-        echo die(json_encode(array('message' => 'ERROR', 'code' => 1337)));
-    }
-    exit();
-    
-}
-
-if(isset($serie) && empty($select)){
-
-//Search Series by name
-    $result = $conn -> prepare("SELECT series.id, series.name, series.genre, series.seasons, series.synopsis, series.age_range, series.lauch_year, series.main_cast, CAST(AVG(note_serie) AS DECIMAL(10,2)) as rating
-    FROM series ,users_series WHERE series.id = users_series.serie_id AND lower(series.name) = lower(?) GROUP BY serie_id");
-    $result -> bindParam(1,$serie);
-    $result -> execute();
-    $ser = $result -> fetch();
-    echo json_encode($ser);
-    exit();
+  <footer class="page-footer teal2">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Sobre</h5>
+          <p class="grey-text text-lighten-4">Site criado pelos alunos do 3º período do Instituto Federal de Pernambuco, curso Informática para Internet.</p>
+           <a href="#inicio" class="return">Voltar ao topo</a>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+        Instituto Federal de Pernambuco - 2019.
+      </div>
+    </div>
+  </footer>
 
 
-}
-if(isset($select) && empty($serie)){
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
 
-    $result = $conn -> prepare("SELECT series.id, series.name, series.genre, series.seasons, series.synopsis, series.age_range, series.lauch_year, series.main_cast, CAST(AVG(note_serie) AS DECIMAL(10,2)) as rating
-    FROM series ,users_series WHERE series.id = users_series.serie_id AND lower(series.genre) = lower(?) GROUP BY serie_id");
-    $result -> bindParam(1,$select);
-    $result -> execute();
-    $ser = $result -> fetch();
-    
-    echo json_encode($ser);
-    //var_dump($ser);
-    exit();
-    
-}
+  </body>
+</html>
 
-
-//echo json_encode($ser);
-
-
-
-?>
